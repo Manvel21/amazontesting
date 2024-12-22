a7b3b70e6d298619520941cf9020f4af9b9fceb4
Manvel21 <manvel.muradyan@edu.ysu.am>
2024-12-22 18:56:53 +0400

    Update DealsPage.cy.js

--- DealsPage.cy.js
+++ DealsPage.cy.js
@@ -1,14 +1,14 @@
 export class DealsPage {
   visit() {
-    cy.visit('https://www.amazon.com');  // Այց amazon-ի գլխավոր էջ
+    cy.visit('https://www.amazon.com');  // Visit the Amazon homepage
   }
 
   clickTodaysDeals() {
-    cy.get('a[href="/gp/goldbox?ref_=nav_cs_gb"]').click({ force: true }); 
+    cy.get('a[href="/gp/goldbox?ref_=nav_cs_gb"]').click({ force: true }); // Force click if overlay is blocking
   }
 
   verifyDealsPage() {
-    cy.url().should('include', '/gp/goldbox');  //Համոզվենք որ գտնվում ենք գործարքների էջում
-    cy.get('.a-declarative').should('be.visible');  // Համոզվեք, որ գործարքների հատվածը տեսանելի է
+    cy.url().should('include', '/gp/goldbox');  // Verify we are on the 'Today's Deals' page
+    cy.get('.a-declarative').should('be.visible');  // Ensure deals section is visible
   }
 }
