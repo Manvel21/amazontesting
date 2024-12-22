707f72b8b48092f87a797a8cd05361bd91174efc
Manvel21 <manvel.muradyan@edu.ysu.am>
2024-12-22 18:47:37 +0400

    Update SignupPage.cy.js

--- SignupPage.cy.js
+++ SignupPage.cy.js
@@ -4,23 +4,23 @@ export class RegisterPage {
   }
 
   clickRegisterButton() {
-    cy.get('#nav-link-accountList').trigger('mouseover'); // Hover over account menu
-    cy.contains('Start here.').click(); // Click "Start here" for registration
+    cy.get('#nav-link-accountList').trigger('mouseover'); 
+    cy.contains('Start here.').click(); 
   }
 
   fillName(name) {
-    cy.get('#ap_customer_name').type(name); // Name input
+    cy.get('#ap_customer_name').type(name); 
   }
 
   fillEmail(email) {
-    cy.get('#ap_email').type(email); // Email input
+    cy.get('#ap_email').type(email); 
   }
 
   fillPassword(password) {
-    cy.get('#ap_password').type(password); // Password input
+    cy.get('#ap_password').type(password); 
   }
 
   clickContinue() {
-    cy.get('#continue').click(); // Continue button
+    cy.get('#continue').click();
   }
 }
