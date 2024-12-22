15e7d45bce47ba699dcbe697869e44720cf76991
Manvel21 <manvel.muradyan@edu.ysu.am>
2024-12-22 18:44:49 +0400

    Create SearchPage-copy-1.cy.js

---
+++ SearchPage-copy-1.cy.js
@@ -0,0 +1,14 @@
+export class SearchPage {
+  visit() {
+    cy.visit('https://www.amazon.com');
+  }
+
+  searchProduct(productName) {
+    cy.get('#twotabsearchtextbox').type(productName); // Search input
+    cy.get('#nav-search-submit-button').click(); // Search button
+  }
+
+  verifyResults() {
+    cy.get('.s-search-results').should('be.visible'); // Search results container
+  }
+}
